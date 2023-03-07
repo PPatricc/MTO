c7bbd737c1d89760b144a639fa67b41586952168
Patryk Paś <86026100+PPatricc@users.noreply.github.com>
2023-03-07 18:55:09 +0100

    Updated wrong main

--- lab2/main.py
+++ lab2/main.py
@@ -3,23 +3,23 @@
 import sys
 
 def my_printf(format_string,param):
-    #print(format_string)
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#' and format_string[idx+1] == 'k':
-                print(param.swapcase(),end="")
-                shouldDo=False
-            elif format_string[idx] == '#' and format_string[idx+1] == '.' and format_string[idx+2].isnumeric() and format_string[idx+3] == 'k':
-            	print(param[0:int(format_string[idx+2])].swapcase(),end="")
-            	shouldDo=False
-            else:
-                print(format_string[idx],end="")
+    leng = len(format_string)
+    i=0
+    while(i<leng):
+        if(format_string[i] == '#' and format_string[i+1]=='.' and format_string[i+2].isdigit and format_string[i+3]=='k'):
+            print(param[0:int(format_string[i+2])].swapcase(), end="")
+            i=i+4
+        elif(format_string[i] == '#' and format_string[i+1]=='k'):
+            print(param.swapcase(),end="")
+            i=i+2
         else:
-            shouldDo=True
+            print(format_string[i], end="")
+            i=i+1
     print("")
-
+    
+    
 data=sys.stdin.readlines()
 
 for i in range(0,len(data),2):
     my_printf(data[i].rstrip(),data[i+1].rstrip())
+
