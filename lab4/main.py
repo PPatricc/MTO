785838bec3e3720d75ec5f8528e5685d37908560
Patryk Paś <86026100+PPatricc@users.noreply.github.com>
2023-03-28 17:29:59 +0200

    Updating main code file

--- lab4/main.py
+++ lab4/main.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 import sys
+import re
+
+
+
+def my_printf(format_string,param):
+    #print(format_string)
+    shouldDo=True
+    for idx in range(0,len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#' and format_string[idx+1] == 'g':
+            	if param.isnumeric():
+            		param = str(param)[::-1]
+            		print(param,end="")
+            		shouldDo=False
+            else:
+                print(format_string[idx],end="")
+        else:
+            shouldDo=True
+    print("")
+
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
 
