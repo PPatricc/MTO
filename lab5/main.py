652ee525914bdfca7abc9461781b0a66dd4ba50b
Patryk Paś <86026100+PPatricc@users.noreply.github.com>
2023-04-04 17:40:59 +0200

    Updated main code file

--- lab5/main.py
+++ lab5/main.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 
 import sys
-import re
+
+def my_printf(format_string,param):
+    i = 0
+    
+    while i < len(format_string):
+        if format_string[i] == "#":
+            width = 0
+            j = i + 1
+            while j < len(format_string) and format_string[j].isdigit():
+                width = width * 10 + int(format_string[j])
+                j += 1
+            if format_string[j] == "g" and param.isalnum():
+                result = ""
+                for c in param:
+                    if c == "0":
+                        result += "9"
+                    else:
+                    	if c.isalnum():
+                            result += str(int(c)-1)
+                if width > len(param):
+                    spaces = " " * (width - len(param))
+                    result = spaces + result
+                i = j + 1
+                print(result, end="")
+            else:
+            	print(format_string[i],end="")
+            	i += 1
+        else:
+            print(format_string[i],end="")
+            i += 1
+                        
+    print("")
 
 
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
