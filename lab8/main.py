0075f1125046bc805eb4f3e7e57f6d5119e514cd
PPatricc <paspatryk12@gmail.com>
2023-05-16 17:28:06 +0200

    Added template test files

---
+++ lab8/main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+    
+def my_printf(format_string,param):
+    if int(param) != 0:
+        param = hex(int(param))
+        param = str(param).lstrip("0x")     
+    
+    param = param.replace("a","g")
+    param = param.replace("b","h")
+    param = param.replace("c","i")
+    param = param.replace("d","j")
+    param = param.replace("e","k")
+    param = param.replace("f","l")
+    prt = re.sub("#j", param, format_string)
+    print(prt)
+
+
+
+data=sys.stdin.readlines()
+
+for i in range(0,len(data),2):
+    my_printf(data[i].rstrip(),data[i+1].rstrip())
