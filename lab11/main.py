4a91a57a7bee2e695f1cc84747bc6d9eeb216abf
PPatricc <paspatryk12@gmail.com>
2023-06-06 17:16:36 +0200

    First changes to exercise

--- lab11/main.py
+++ lab11/main.py
@@ -3,27 +3,12 @@
 import sys
 import re
 
-def change_num(x):
-    new_num = int((x*2)/len(str(abs(x))))
-    return new_num
- 
- 
- 
-def change_odd(x):
-    if x % 2 != 0:
-        p = hex(x)
-        rep = str(p).lstrip("0x") 
-        return rep
-    return x   
-
 
 def my_printf(format_string,param):
-    if '#a' not in format_string:
+    if '#b' not in format_string:
          print(format_string)
          return
-    
-    new_N = change_num(int(param))
-    to_print = change_odd(new_N)
+
     
     print(format_string.replace('#a',str(to_print)))
 
